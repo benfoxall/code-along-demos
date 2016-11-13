0997ffe933c0c4cdb98133f7abb1b8fa7fdc6152
Ben Foxall <ben@pusher.com>
2016-11-13 20:05:25 +0000

    draw waveform

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ import {play, analyser} from 'audio.js'
 import {onSubmit} from 'form.js'
 
 onSubmit( play )
+
+const canvas = document.createElement('canvas')
+canvas.style.position = 'static'
+canvas.style.display = 'block'
+document.body.appendChild(canvas)
+
+const ctx = canvas.getContext('2d')
+ctx.translate(0, canvas.height/2)

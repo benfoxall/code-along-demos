253ffa514ca74761763d1787078a76b5eb69ee0e
Ben Foxall <ben@pusher.com>
2016-11-13 19:00:17 +0000

    add a play button

--- main.js
+++ main.js
@@ -5,4 +5,8 @@ freq.type = 'number'
 freq.value = 440
 document.body.appendChild(freq)
 
+const start = document.createElement('button')
+start.innerText = 'play'
+document.body.appendChild(start)
+
 //play(440)

ebbbf73bdb684560bea211f5571b8449d3ab5587
Ben Foxall <ben@pusher.com>
2016-11-13 19:36:30 +0000

    extract form to module

---
+++ form.js
@@ -0,0 +1,14 @@
+const freq = document.createElement('input')
+freq.type = 'number'
+freq.value = 440
+document.body.appendChild(freq)
+
+const start = document.createElement('button')
+start.innerText = 'play'
+document.body.appendChild(start)
+
+export const onSubmit = (fn) => {
+  start.addEventListener('click', e =>
+    fn(freq.value)
+  )
+}
--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 import {play} from 'audio.js'
+import {onSubmit} from 'form.js'
 
-const freq = document.createElement('input')
-freq.type = 'number'
-freq.value = 440
-document.body.appendChild(freq)
-
-const start = document.createElement('button')
-start.innerText = 'play'
-document.body.appendChild(start)
-
-const onSubmit = (fn) => {
-  start.addEventListener('click', e =>
-    fn(freq.value)
-  )
-}
-
-onSubmit( f => play(f) )
+onSubmit( play )

029eb929c2c78fc56f31107bf60c79a7638128ef
Ben Foxall <ben@pusher.com>
2016-11-13 20:06:01 +0000

    implement drawWave

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ document.body.appendChild(canvas)
 
 const ctx = canvas.getContext('2d')
 ctx.translate(0, canvas.height/2)
+
+const drawWave = data => {
+  ctx.clearRect(0,-canvas.height/2, canvas.width, canvas.height)
+  ctx.beginPath()
+  for(var i = 0; i < data.length; i++) {
+    ctx.lineTo(
+      (canvas.width/data.length*i),
+      (data[i]*10)
+    )
+  }
+  ctx.stroke()
+}

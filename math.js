ae2711a29666b5a916ca7e1d55c4ae5c98cae280
Ben Foxall <ben@pusher.com>
2016-11-14 14:56:19 +0000

    update rotate matrices to be 3x3

--- math.js
+++ math.js
@@ -1,9 +1,11 @@
 export const scale = s => $M([
-  [s, 0],
-  [0, s]
+  [s, 0, 0],
+  [0, s, 0],
+  [0, 0, s]
 ])
 
 export const rotate = theta => $M([
-  [Math.cos(theta), -Math.sin(theta)],
-  [Math.sin(theta), Math.cos(theta)],
+  [Math.cos(theta), -Math.sin(theta), 0],
+  [Math.sin(theta),  Math.cos(theta), 0],
+  [0,                0,               1],
 ])

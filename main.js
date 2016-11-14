e87568c51e556609dd643177c71b1066f3606ad4
Ben Foxall <ben@pusher.com>
2016-11-14 09:37:36 +0000

    use matrices

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
+//+https://cdnjs.cloudflare.com/ajax/libs/sylvester/0.1.3/sylvester.min.js
+
 import {ctx} from 'draw.js'
 
-const drawPoint = p => ctx.fillRect(p[0]-2, p[1]-2, 4, 4)
+const drawPoint = p => ctx.fillRect(p.e(1)-2, p.e(2)-2, 4, 4)
 
 const points = [
   [-10,-10],
   [-10,+10],
   [+10,+10],
   [+10,-10]
-]
+].map(v => $V(v))
 
 points
   .forEach(drawPoint)

1c1798e3c0a1ef7a4bc5a3074fe5fe592239a36a
Ben Foxall <ben@pusher.com>
2016-11-14 09:34:45 +0000

    add resize listener

--- draw.js
+++ draw.js
@@ -1,14 +1,20 @@
-const ratio = window.devicePixelRatio || 1
 const canvas = document.createElement('canvas')
-const w = canvas.width = window.innerWidth * ratio
-const h = canvas.height = window.innerHeight * ratio
-
-canvas.style.width = `${window.innerWidth}px`
-canvas.style.height = `${window.innerHeight}px`
+export const ctx = canvas.getContext('2d')
 document.body.appendChild(canvas)
 
-export const ctx = canvas.getContext('2d')
-ctx.translate(canvas.width/2,canvas.height/2)
-ctx.scale(ratio,ratio)
+let ratio, w, h
 
-ctx.rotate(1)
+const size = _ => {
+  ratio = window.devicePixelRatio || 1
+
+  w = canvas.width = window.innerWidth * ratio
+  h = canvas.height = window.innerHeight * ratio
+
+  canvas.style.width = `${window.innerWidth}px`
+  canvas.style.height = `${window.innerHeight}px`
+  ctx.translate(canvas.width/2,canvas.height/2)
+  ctx.scale(ratio,ratio)
+}
+size()
+
+window.addEventListener('resize', size)

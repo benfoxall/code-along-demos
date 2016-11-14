9a13a2990de0e4cdccf7460a4cce83ba32ab03b0
Ben Foxall <ben@pusher.com>
2016-11-14 09:04:05 +0000

    extract canvas out

---
+++ draw.js
@@ -0,0 +1,7 @@
+const canvas = document.createElement('canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+document.body.appendChild(canvas)
+
+export const ctx = canvas.getContext('2d')
+ctx.translate(canvas.width/2,canvas.height/2)

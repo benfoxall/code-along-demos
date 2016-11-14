77f644d4ed830eb2a2f68c0932ef1d909ee5f0b9
Ben Foxall <ben@pusher.com>
2016-11-14 09:20:59 +0000

    Make use of pixel ratio to scale down canvas

--- draw.js
+++ draw.js
@@ -1,9 +1,14 @@
+const ratio = window.devicePixelRatio || 1
 const canvas = document.createElement('canvas')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const w = canvas.width = window.innerWidth * ratio
+const h = canvas.height = window.innerHeight * ratio
+
+canvas.style.width = `${window.innerWidth}px`
+canvas.style.height = `${window.innerHeight}px`
 document.body.appendChild(canvas)
 
 export const ctx = canvas.getContext('2d')
 ctx.translate(canvas.width/2,canvas.height/2)
+ctx.scale(ratio,ratio)
 
 ctx.rotate(1)
